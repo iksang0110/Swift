01e2b42c09338c20544cd11b36b97c411b1a4812
Iksang_J <wjddlrtkd0110@naver.com>
2021-11-08 20:45:32 +0900

    파일 정리

---
+++ One Piece Prize1/One piece Prize/Bounty/BountyViewController.swift
@@ -0,0 +1,62 @@
+import UIKit
+
+class BountyViewController: UIViewController, UICollectionViewDataSource ,UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    let viewModel = BountyViewModel()
+    
+    //DataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.numberOfBountyList()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridCell", for: indexPath) as? GridCell else {
+            return UICollectionViewCell()
+        }
+        let bountyInfo_idx = viewModel.bountyInfoOfIndex(index: indexPath.item)
+        cell.setCell(info: bountyInfo_idx)
+        return cell
+    }
+    
+    //Delegate
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showDetail", sender: indexPath.item)
+    }
+    
+    //Delegate Flow Layout
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellSpacing : CGFloat = 10
+        let textArea : CGFloat = 65
+        
+        let width : CGFloat = (collectionView.bounds.width - cellSpacing) / 2
+        let height : CGFloat = width * (10/7) + textArea
+        
+        return CGSize(width: width, height: height)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail"{
+            let vc = segue.destination as? DetailViewController
+            if let index = sender as? Int{
+                let bountyInfo = viewModel.bountyInfoOfIndex(index: index)
+                vc?.viewModel.update(model: bountyInfo)
+            }
+        }
+    }
+}
+
+class GridCell: UICollectionViewCell{
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLabel: UILabel!
+    
+    func setCell(info : BountyInfo){
+        imgView?.image = info.getImageInfo()
+        nameLabel.text = info.name
+        bountyLabel.text = "\(info.bounty)"
+    }
+}
---
+++ One Piece Prize1/One piece Prize/DetailView/DetailViewController.swift
@@ -0,0 +1,81 @@
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLabel: UILabel!
+    @IBOutlet weak var nameLabelCenterX: NSLayoutConstraint!
+    @IBOutlet weak var bountyLabelCenterX: NSLayoutConstraint!
+
+    let viewModel = DetailViewModel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setView()
+        prepareAnimation()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        //현상금에 따라 애니메이션 다르게 설정해보기
+        if let bounty = viewModel.bountyInfo{
+            if bounty.bounty >= 10000000{
+                showHighBounty();
+            }else{
+                showAnimation()
+            }
+        }
+    }
+    
+    private func showHighBounty(){
+        UIView.transition(with: image, duration: 0.3, options: .transitionCurlDown, animations: nil, completion: nil)
+        UIView.animate(withDuration: 0.3, delay: 0.2, usingSpringWithDamping: 1.0, initialSpringVelocity: 3, options:.allowUserInteraction, animations: {
+            self.nameLabel.transform = CGAffineTransform.identity
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.3, delay: 0.5, usingSpringWithDamping: 3.0, initialSpringVelocity: 1, options: .allowUserInteraction, animations: {
+            self.bountyLabel.transform = CGAffineTransform.identity
+        }, completion: nil)
+        
+        nameLabel.alpha = 1
+        bountyLabel.alpha = 1
+    }
+    
+    private func prepareAnimation(){
+        nameLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        bountyLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        
+        nameLabel.alpha = 0
+        bountyLabel.alpha = 0
+    }
+    
+    private func showAnimation(){
+        UIView.transition(with: image, duration: 0.3 , options: .transitionFlipFromLeft, animations: nil, completion: nil)
+        
+        UIView.animate(withDuration: 0.5, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            //변형을 가하기 전의 상태
+            self.nameLabel.transform = CGAffineTransform.identity
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.5, delay: 0.4, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            //변형을 가하기 전의 상태
+            self.bountyLabel.transform = CGAffineTransform.identity
+        }, completion: nil)
+        
+        nameLabel.alpha = 1
+        bountyLabel.alpha = 1
+    }
+    
+    @IBAction func closeButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func setView(){
+        if let bountyInfo = viewModel.bountyInfo{
+            image.image = UIImage(named: "\(bountyInfo.name).jpg")
+            nameLabel.text = bountyInfo.name
+            bountyLabel.text = "\(bountyInfo.bounty)"
+        }
+    }
+}
+
